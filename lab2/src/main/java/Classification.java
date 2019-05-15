43bb041192b79fcee8ca3075b8399891cefe006c
Mark <f18846188605@gmail.com>
2019-05-15 21:23:20 +0800

    trying

--- lab2/src/main/java/Classification.java
+++ lab2/src/main/java/Classification.java
@@ -2,46 +2,59 @@ import java.util.*;
 
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.Function;
+import org.apache.spark.sql.AnalysisException;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 
-import scala.Tuple2;
-
 public class Classification {
-    public static void main(String[] args) {
+    static int DEGREE = 0;
+
+    public static void main(String[] args) throws AnalysisException {
         SparkSession spark = SparkSession.builder().appName("first").master("local[*]")
                 .config("spark.some.config.option", "some-value").getOrCreate();
 
         JavaSparkContext sc = JavaSparkContext.fromSparkContext(spark.sparkContext());
         sc.setLogLevel("ERROR");
         String filePath = "./SUSY.csv";
-        JavaRDD<String> fileInput = sc.textFile(filePath);
-        JavaRDD<BayesRecord> dataset = fileInput.map(r -> new BayesRecord(r));
-        Dataset<Row> rawData = spark.createDataFrame(dataset, BayesRecord.class);
-        Dataset<Row> trainData = rawData.sample(false, 0.8, 1000);
+        // JavaRDD<String> fileInput = sc.textFile(filePath);
+        Dataset<Row> inputData = spark.read().format("com.databricks.spark.csv").option("header", false)
+                .option("delimiter", ",").option("inferSchema", true).load(filePath);
+        // inputData.printSchema();
+        DEGREE = inputData.columns().length - 1;
+        inputData.createTempView("records");
+
+        Dataset<Row> trainData = inputData.sample(false, 0.0001, 1000);
         // Dataset<Row> testData = rawData.sample(false, 0.2, 1000);
-        System.out.println(prioriProbability(trainData).toString());
-        // test.takeAsList(10).forEach(System.out::println);
-        // String filePath = "./USCensus1990.data.txt";
-        // JavaRDD<String> fileRDD = sc.textFile(filePath);
-        // JavaRDD<Record> parseData = fileRDD.map(S -> new Record(S));
-        // parseData.map(r -> r.getType()).repartition(1).saveAsTextFile("./result");
+        prioriProbability(spark, trainData);
         spark.stop();
     }
 
-    static Map<Integer, Double> prioriProbability(Dataset<Row> trainData) {
+    static Map<Integer, Double> prioriProbability(SparkSession spark, Dataset<Row> trainData) {
         long totalNum = trainData.count();
-        Dataset<Row> prioriProbability = trainData.groupBy("type").count().selectExpr("type", "count/" + totalNum)
-                .persist();
+        String sql = "select _c0,";
+        // for (int i = 1; i <= DEGREE; i++) {
+        // sql += "mean(_c" + i + "),";
+        // sql += "stddev(_c" + i + "),";
+        // }
+        sql += "count(_c1)/" + totalNum + " as p from records group by _c0";
+        Dataset<Row> prioriProbability = spark.sql(sql).persist();
+        prioriProbability.show();
         int typeNum = (int) prioriProbability.count();
-        List<Tuple2<Integer, Double>> midResult = prioriProbability.toJavaRDD()
-                .map(r -> new Tuple2<Integer, Double>((int) r.get(0), (double) r.get(1))).take(typeNum);
-        prioriProbability.unpersist();
-        HashMap<Integer, Double> Result = new HashMap<>();
-        for (Tuple2<Integer, Double> t : midResult) {
-            Result.put((int) t._1(), (double) t._2());
-        }
-        return Result;
+        JavaRDD<List<Double>> midResult = prioriProbability.toJavaRDD().map(new Function<Row, List<Double>>() {
+            public List<Double> call(Row key) throws Exception {
+                List<Double> result = new ArrayList<>();
+                result.add(key.getDouble(0));
+                result.add(key.getDouble(1));
+                return result;
+            }
+        });
+        midResult.take(typeNum);
+        System.out.println(midResult.toString());
+        // prioriProbability.unpersist();
+        return null;
     }
+
+    // static
 }
