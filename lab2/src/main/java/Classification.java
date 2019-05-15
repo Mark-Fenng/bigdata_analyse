287db33b6a28b919ccdd67f7cc289d08a26d9d99
Mark <f18846188605@gmail.com>
2019-05-16 00:23:37 +0800

    trying

--- lab2/src/main/java/Classification.java
+++ lab2/src/main/java/Classification.java
@@ -23,38 +23,65 @@ public class Classification {
                 .option("delimiter", ",").option("inferSchema", true).load(filePath);
         // inputData.printSchema();
         DEGREE = inputData.columns().length - 1;
-        inputData.createTempView("records");
 
-        Dataset<Row> trainData = inputData.sample(false, 0.0001, 1000);
-        // Dataset<Row> testData = rawData.sample(false, 0.2, 1000);
-        prioriProbability(spark, trainData);
+        Dataset<Row> trainData = inputData.sample(false, 0.8, 1000);
+        System.out.println("Start training");
+        List<List<Double>> BayesModel = prioriProbability(spark, trainData);
+        System.out.println("Start testing");
+        Dataset<Row> testData = inputData.sample(false, 0.2, 2000);
+        long testTotal = testData.count();
+        int result = testData.toJavaRDD().map(new Function<Row, Integer>() {
+            private static final long serialVersionUID = 1L;
+
+            @Override
+            public Integer call(Row key) throws Exception {
+                double max = -10000;
+                int max_index = -1;
+                for (int i = 0; i < BayesModel.size(); i++) {
+                    double r = 1;
+                    for (int j = 0; j < DEGREE; j++) {
+                        r = r * (1 / (Math.sqrt(2 * Math.PI) * BayesModel.get(i).get(1 + 2 * j + 1))
+                                * Math.exp(-(Math.pow(key.getDouble(1 + j) - BayesModel.get(i).get(1 + 2 * j), 2) / 2
+                                        / Math.pow(BayesModel.get(i).get(1 + 2 * j + 1), 2))));
+                    }
+                    r *= BayesModel.get(i).get(2 * (DEGREE + 1) - 1);
+                    if (r > max) {
+                        max_index = i;
+                    }
+                }
+                return (int) key.getDouble(0) == max_index ? 1 : 0;
+            }
+        }).reduce((a, b) -> a + b);
+        System.out.println(result / (double) testTotal);
         spark.stop();
     }
 
-    static Map<Integer, Double> prioriProbability(SparkSession spark, Dataset<Row> trainData) {
+    static List<List<Double>> prioriProbability(SparkSession spark, Dataset<Row> trainData) throws AnalysisException {
+        trainData.createTempView("records");
         long totalNum = trainData.count();
         String sql = "select _c0,";
-        // for (int i = 1; i <= DEGREE; i++) {
-        // sql += "mean(_c" + i + "),";
-        // sql += "stddev(_c" + i + "),";
-        // }
+        for (int i = 1; i <= DEGREE; i++) {
+            sql += "mean(_c" + i + "),";
+            sql += "stddev(_c" + i + "),";
+        }
         sql += "count(_c1)/" + totalNum + " as p from records group by _c0";
         Dataset<Row> prioriProbability = spark.sql(sql).persist();
-        prioriProbability.show();
+        // prioriProbability.show();
         int typeNum = (int) prioriProbability.count();
         JavaRDD<List<Double>> midResult = prioriProbability.toJavaRDD().map(new Function<Row, List<Double>>() {
+            private static final long serialVersionUID = 1L;
+
+            @Override
             public List<Double> call(Row key) throws Exception {
                 List<Double> result = new ArrayList<>();
-                result.add(key.getDouble(0));
-                result.add(key.getDouble(1));
+                for (int i = 0; i < 2 * (DEGREE + 1); i++) {
+                    result.add(key.getDouble(i));
+                }
                 return result;
             }
         });
-        midResult.take(typeNum);
-        System.out.println(midResult.toString());
-        // prioriProbability.unpersist();
-        return null;
+        List<List<Double>> result = midResult.take(typeNum);
+        prioriProbability.unpersist();
+        return result;
     }
-
-    // static
 }
